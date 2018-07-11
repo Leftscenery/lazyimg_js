fe2c9b0e93a15ae513c6d0ec9521210a2fb60968
leftscenery <leftscenery@163.com>
2018-07-11 15:17:37 +0800

    fix transition

--- lazy_img_js.js
+++ lazy_img_js.js
@@ -3,21 +3,19 @@
     把需要加载的图片用<img src="../resource/10.jpg" alt="">方式加载到DOM中
     把需要延迟加载的图片放到class：lazyImg里
     激活
-        LazyImg.load({
-            loading_type:'bottom',
-            loading_img: 'xxxx.jpg',
-            loading_transition:false,
-            loading_time: '0.5s'
-        });
+    LazyImg.load({
+        loading_type:'bottom',
+        loading_img: 'xxxx.jpg',
+        loading_transition:false,
+        loading_time: 500
+    });
 
 参数说明：
     loading_type：加载位置，当图片进入到哪里时候开始加载图片，有三个参数：bottom,top,middle
     loading_img：加载图片地址
     loading_transition：是否开启渐变加载
-    loading_time：渐变加载时间
+    loading_time：渐变加载时间，毫秒
 */
-
-document.write("<script src='utils.js'></script>");
 
 let LazyImg = (function () {
     var isCompatible;
@@ -27,10 +25,11 @@ let LazyImg = (function () {
     var winScrollTop;
     var plan;
     var _default;
+
     //函数赋值
-    function initData(){
+    function initData() {
         isCompatible = ('getComputedStyle' in window);
-        holder = utils.getElementByClass('lazyImg');
+        holder = getElementByClass('lazyImg');
         winHeight = document.documentElement.clientHeight || document.body.clientHeight;
         winWidth = document.documentElement.clientWidth || document.body.clientWidth;
         winScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
@@ -39,7 +38,7 @@ let LazyImg = (function () {
             loading_type: 'bottom',
             loading_img: 'https://img.zcool.cn/community/0142eb5992b0650000002129b61cfc.gif',
             loading_transition: true,
-            loading_time: '0.5s'
+            loading_time: 500
         };
     }
 
@@ -58,7 +57,7 @@ let LazyImg = (function () {
     }
 
     function getTargetLine(ele) {
-        var baseLine = utils.offset(ele).top;
+        var baseLine = offset(ele).top;
         var eleHeight = ele.offsetHeight;
         switch (_default.loading_type) {
             case 'top':
@@ -84,37 +83,66 @@ let LazyImg = (function () {
         }
     }
 
-    function fade(ele,src) {
-        let op = window.getComputedStyle(ele,null)['opacity'];
+    function fade(ele, src) {
+        let op = Number(window.getComputedStyle(ele, null)['opacity']);
+        let step = 1/(_default.loading_time/17);
+        console.log(step);
         ele.style.opacity = op;
         let timer = setInterval(function () {
-            if(ele.style.opacity<=0){
+            if (ele.style.opacity <= 0) {
                 clearInterval(timer);
                 ele.setAttribute('src', src);
                 ele.removeAttribute('img-data');
                 ele.style.background = '';
                 ele.style.opacity = 0;
-                ele.style.transition = `all ${_default.loading_time}`;
-                ele.style.opacity = 1;
-            }else{
+                //渐显
+                let timer1 = setInterval(function () {
+                    if (ele.style.opacity >= 1) {
+                        ele.style.opacity = 1;
+                        clearInterval(timer1);
+                    }else{
+                        ele.style.opacity = Number(ele.style.opacity) + step;
+                    }
+                }, 17);
+            } else {
                 ele.style.opacity -= 0.2;
             }
-        },17)
+        }, 17)
     }
-    var offset = function offset (ele){
+
+
+    var offset = function offset(ele) {
         let L = ele.offsetLeft;
         let T = ele.offsetTop;
         let parent = ele.offsetParent;
-        while(parent){
-            if(!/MSIE 8/.test(window.navigator.userAgent)){
-                L+=parent.clientLeft;
-                T+=parent.clientTop;
+        while (parent) {
+            if (!/MSIE 8/.test(window.navigator.userAgent)) {
+                L += parent.clientLeft;
+                T += parent.clientTop;
             }
-            L+=parent.offsetLeft;
-            T+=parent.offsetTop;
-            parent= parent.offsetParent;
+            L += parent.offsetLeft;
+            T += parent.offsetTop;
+            parent = parent.offsetParent;
         }
-        return {left:L,top:T}
+        return {left: L, top: T}
+    };
+
+    //GetByClassName
+    var getElementByClass = function getElementByClass(str, context) {
+        context = context || document;
+        if ('getElementsByClassName' in document) {
+            return [...context.getElementsByClassName(str)];
+        }
+        //IE 6-8兼容
+        str = str.trim().replace(/ +/g, '|');
+        var result = []
+        var childrenList = context.getElementsByTagName('*');
+        reg = new RegExp('(^| +)(' + str + ')( +|$)');
+        console.log(reg);
+        for (var i = 0; i < childrenList.length; i++) {
+            reg.test(childrenList[i].className) ? result.push(childrenList[i]) : null;
+        }
+        return result
     };
 
     //把img打包在div内部，替换默认加载图片，并把真实图片地址放到img-data属性下
@@ -125,8 +153,8 @@ let LazyImg = (function () {
                 //设置属性
                 var item = imgList[j];
                 var src = imgList[j].getAttribute('src');
-                item.setAttribute('src',"");
-                item.setAttribute('img-data',src);
+                item.setAttribute('src', "");
+                item.setAttribute('img-data', src);
                 item.style.background = `url(${_default.loading_img}) no-repeat center`;
                 item.style.backgroundSize = 'contain';
                 imgList[j].flg = false;
@@ -140,21 +168,21 @@ let LazyImg = (function () {
     function lazyLoad() {
         updateScroll();
         for (let i = 0; i < plan.length; i++) {
-            if(!plan[i].flg){
+            if (!plan[i].flg) {
                 var targetLine = getTargetLine(plan[i]);
                 if ((winScrollTop + winHeight) >= targetLine) {
                     let newImg = new Image();
                     let src = plan[i].getAttribute('img-data');
                     newImg.src = src;
                     newImg.onload = function () {
-                        if(_default.loading_transition){
-                            fade(plan[i],src);
-                        }else{
+                        plan[i].flg = true;
+                        if (_default.loading_transition) {
+                            fade(plan[i], src);
+                        } else {
                             plan[i].setAttribute('src', src);
                             plan[i].removeAttribute('img-data');
                             plan[i].style.background = '';
                         }
-                        plan[i].flg = true;
                     };
                     newImg = null;
                 }
@@ -162,11 +190,17 @@ let LazyImg = (function () {
         }
     }
 
-    //载入window onscroll函数
+    //载入window 监听
     function loadEvent() {
         addEvent(window, 'scroll', function () {
             lazyLoad();
         });
+        addEvent(window, 'resize', function () {
+            lazyLoad();
+        });
+        addEvent(window, 'load', function () {
+            lazyLoad();
+        })
     }
 
     //更改inline
@@ -179,13 +213,13 @@ let LazyImg = (function () {
         }
     }
 
+    //返回
     return {
         load: function (options) {
             initData();
             changeDefault(options);
             inlineDetect();
             replaceImg();
-            lazyLoad();
             loadEvent();
         }
     }
